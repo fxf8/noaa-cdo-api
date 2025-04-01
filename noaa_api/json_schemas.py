655755de5a486a373b64983e4b10bddc81574d59
Fuexfollets <fuex9follets@gmail.com>
2025-04-01 12:14:20 -0400

    Added doctrings to json_schmeas

--- noaa_api/json_schemas.py
+++ noaa_api/json_schemas.py
@@ -2,6 +2,15 @@ from typing import NotRequired, TypedDict
 
 
 class ResultSetJSON(TypedDict):
+    """
+    Represents metadata about the result set, including pagination details.
+
+    Attributes:
+        offset (int): The starting point of the returned results.
+        count (int): The total number of results available.
+        limit (int): The maximum number of results returned per request.
+    """  # noqa: E501
+
     offset: int
     count: int
     limit: int
@@ -9,11 +18,26 @@ class ResultSetJSON(TypedDict):
 
 # --- Full JSON Response Structure ---
 class MetadataJSON(TypedDict):
+    """
+    Contains metadata information for a response, including result set details.
+
+    Attributes:
+        resultset (ResultSetJSON): Pagination and count details of the results.
+    """  # noqa: E501
+
     resultset: ResultSetJSON
 
 
 # Rate limit json reponse
 class RateLimitJSON(TypedDict):
+    """
+    Represents the JSON response structure for rate limit information.
+
+    Attributes:
+        status (str): The status of the rate limit (e.g., 'error').
+        message (str): A descriptive message regarding the rate limit status.
+    """  # noqa: E501
+
     status: str
     message: str
 
@@ -24,8 +48,15 @@ class RateLimitJSON(TypedDict):
 # --- Full JSON Response Structure ---
 class DatasetIDJSON(TypedDict):
     """
-    Full JSON Response for endpoint '/datasets/{id}'
-    """
+    Endpoint '/datasets/{id}'
+    Represents the JSON response structure for a specific dataset identified by its ID.
+
+    Attributes:
+        mindate (str): The earliest date for which data is available, formatted as 'YYYY-MM-DD'.
+        maxdate (str): The latest date for which data is available, formatted as 'YYYY-MM-DD'.
+        name (str): The name of the dataset. datacoverage (float | int): The proportion of data coverage, ranging from 0 to 1.
+        id (str): The unique identifier for the dataset.
+    """  # noqa: E501
 
     mindate: str  # Date as "YYYY-MM-DD"
     maxdate: str  # Date as "YYYY-MM-DD"
@@ -39,8 +70,16 @@ class DatasetIDJSON(TypedDict):
 
 class DatasetJSON(TypedDict):
     """
-    JSON Subcomponent for endpoint '/datasets'
-    """
+    Endpoint '/datasets' (subcomponent)
+    Represents a dataset within the '/datasets' endpoint response. (subcomponent)
+
+    Attributes:
+        uid (str): The unique identifier for the dataset.
+        mindate (str): The earliest date for which data is available, formatted as 'YYYY-MM-DD'.
+        maxdate (str): The latest date for which data is available, formatted as 'YYYY-MM-DD'.
+        name (str): The name of the dataset. datacoverage (float | int): The proportion of data coverage, ranging from 0 to 1.
+        id (str): The unique identifier for the dataset.
+    """  # noqa: E501
 
     uid: str
     mindate: str  # Date as "YYYY-MM-DD"
@@ -53,8 +92,13 @@ class DatasetJSON(TypedDict):
 # --- Full JSON Response Structure ---
 class DatasetsJSON(TypedDict):
     """
-    Full JSON Response for endpoint '/datasets'
-    """
+    Endpoint '/datasets'
+    Represents the JSON response structure for the '/datasets' endpoint.
+
+    Attributes:
+        metadata (MetadataJSON): Metadata information about the response.
+        results (list[DatasetJSON]): A list of datasets returned in the response.
+    """  # noqa: E501
 
     metadata: MetadataJSON
     results: list[DatasetJSON]
@@ -65,8 +109,13 @@ class DatasetsJSON(TypedDict):
 
 class DatacategoryIDJSON(TypedDict):
     """
-    Full JSON Response for endpoint '/datacategories/{id}'
-    """
+    Endpoint '/datacategories/{id}'
+    Represents the JSON response structure for a specific data category identified by its ID.
+
+    Attributes:
+        name (str): The name of the data category.
+        id (str): The unique identifier for the data category.
+    """  # noqa: E501
 
     name: str
     id: str
@@ -77,8 +126,13 @@ class DatacategoryIDJSON(TypedDict):
 
 class DatacategoriesJSON(TypedDict):
     """
-    Full JSON Response for endpoint '/datacategories'
-    """
+    Endpoint '/datacategories'
+    Represents the JSON response structure for the '/datacategories' endpoint.
+
+    Attributes:
+        metadata (MetadataJSON): Metadata information about the response.
+        results (list[DatacategoryIDJSON]): A list of data categories returned in the response.
+    """  # noqa: E501
 
     metadata: MetadataJSON
     results: list[DatacategoryIDJSON]
@@ -89,8 +143,15 @@ class DatacategoriesJSON(TypedDict):
 
 class DatatypeIDJSON(TypedDict):
     """
-    Full JSON Response for endpoint '/datatypes/{id}'
-    """
+    Endpoint '/datatypes/{id}'
+    Represents the JSON response structure for a specific data type identified by its ID.
+
+    Attributes:
+        mindate (str): The earliest date for which the data type is available, formatted as 'YYYY-MM-DD'.
+        maxdate (str): The latest date for which the data type is available, formatted as 'YYYY-MM-DD'.
+        datacoverage (float | int): The proportion of data coverage for the data type.
+        id (str): The unique identifier for the data type.
+    """  # noqa: E501
 
     mindate: str  # Date as "YYYY-MM-DD"
     maxdate: str  # Date as "YYYY-MM-DD"
@@ -103,8 +164,16 @@ class DatatypeIDJSON(TypedDict):
 
 class DatatypeJSON(TypedDict):
     """
-    JSON Subcomponent for endpoint '/datatypes'
-    """
+    Endpoint '/datatypes'
+    Represents a data type within the '/datatypes' endpoint response. (subcomponent)
+
+    Attributes:
+        mindate (str): The earliest date for which the data type is available, formatted as 'YYYY-MM-DD'.
+        maxdate (str): The latest date for which the data type is available, formatted as 'YYYY-MM-DD'.
+        name (str): The name of the data type.
+        datacoverage (float | int): The proportion of data coverage for the data type.
+        id (str): The unique identifier for the data type.
+    """  # noqa: E501
 
     mindate: str  # Date as "YYYY-MM-DD"
     maxdate: str  # Date as "YYYY-MM-DD"
@@ -115,8 +184,13 @@ class DatatypeJSON(TypedDict):
 
 class DatatypesJSON(TypedDict):
     """
-    Full JSON Response for endpoint '/datatypes'
-    """
+    Endpoint '/datatypes'
+    Represents the JSON response structure for the '/datatypes' endpoint.
+
+    Attributes:
+        metadata (MetadataJSON): Metadata information about the response.
+        results (list[DatatypeJSON]): A list of data types returned in the response.
+    """  # noqa: E501
 
     metadata: MetadataJSON
     results: list[DatatypeJSON]
@@ -127,8 +201,13 @@ class DatatypesJSON(TypedDict):
 
 class LocationcategoryIDJSON(TypedDict):
     """
-    Full JSON Response for endpoint '/locationcategories/{id}'
-    """
+    Endpoint '/locationcategories/{id}'
+    Represents the JSON response structure for a specific location category identified by its ID.
+
+    Attributes:
+        name (str): The name of the location category.
+        id (str): The unique identifier for the location category.
+    """  # noqa: E501
 
     name: str
     id: str
@@ -139,8 +218,13 @@ class LocationcategoryIDJSON(TypedDict):
 
 class LocationcategoriesJSON(TypedDict):
     """
-    Full JSON Response for endpoint '/locationcategories'
-    """
+    Endpoint '/locationcategories'
+    Represents the JSON response structure for the '/locationcategories' endpoint.
+
+    Attributes:
+        metadata (MetadataJSON): Metadata information about the response.
+        results (list[LocationcategoryIDJSON]): A list of location categories returned in the response.
+    """  # noqa: E501
 
     metadata: MetadataJSON
     results: list[LocationcategoryIDJSON]
@@ -151,8 +235,16 @@ class LocationcategoriesJSON(TypedDict):
 
 class LocationIDJSON(TypedDict):
     """
-    Full JSON Response for endpoint '/locations/{id}'
-    """
+    Endpoint '/locations/{id}'
+    Represents the JSON response structure for a specific location identified by its ID.
+
+    Attributes:
+        mindate (str): The earliest date for which data is available for the location, formatted as 'YYYY-MM-DD'.
+        maxdate (str): The latest date for which data is available for the location, formatted as 'YYYY-MM-DD'.
+        name (str): The name of the location.
+        datacoverage (float | int): The proportion of data coverage for the location.
+        id (str): The unique identifier for the location.
+    """  # noqa: E501
 
     mindate: str  # Date as "YYYY-MM-DD"
     maxdate: str  # Date as "YYYY-MM-DD"
@@ -166,8 +258,13 @@ class LocationIDJSON(TypedDict):
 
 class LocationsJSON(TypedDict):
     """
-    Full JSON Response for endpoint '/locations'
-    """
+    Endpoint '/locations'
+    Represents the JSON response structure for the '/locations' endpoint.
+
+    Attributes:
+        metadata (MetadataJSON): Metadata information about the response, including pagination details.
+        results (list[LocationIDJSON]): A list of location records returned by the query.
+    """  # noqa: E501
 
     metadata: MetadataJSON
     results: list[LocationIDJSON]
@@ -178,8 +275,20 @@ class LocationsJSON(TypedDict):
 
 class StationIDJSON(TypedDict):
     """
-    Full JSON Response for endpoint '/stations/{id}'
-    """
+    Endpoint '/stations/{id}'
+    Represents the JSON response structure for a specific station identified by its ID from the '/stations/{id}' endpoint.
+
+    Attributes:
+        elevation (float): The elevation of the station in meters.
+        mindate (str): The earliest date for which data is available, formatted as 'YYYY-MM-DD'.
+        maxdate (str): The latest date for which data is available, formatted as 'YYYY-MM-DD'.
+        latitude (float): The latitude coordinate of the station.
+        name (str): The name of the station.
+        datacoverage (float | int): The proportion of data coverage, ranging from 0 to 1.
+        id (str): The unique identifier for the station.
+        elevationUnit (str): The unit of measurement for elevation (e.g., 'METERS').
+        longitude (float): The longitude coordinate of the station.
+    """  # noqa: E501
 
     elevation: float
     mindate: str  # Date as "YYYY-MM-DD"
@@ -197,8 +306,13 @@ class StationIDJSON(TypedDict):
 
 class StationsJSON(TypedDict):
     """
-    Full JSON Response for endpoint '/stations'
-    """
+    Endpoint '/stations'
+    Represents the JSON response structure for the '/stations' endpoint.
+
+    Attributes:
+        metadata (MetadataJSON): Metadata information about the response, including pagination details.
+        results (list[StationIDJSON]): A list of station records returned by the query.
+    """  # noqa: E501
 
     metadata: MetadataJSON
     results: list[StationIDJSON]
@@ -209,8 +323,16 @@ class StationsJSON(TypedDict):
 
 class DatapointJSON(TypedDict):
     """
-    JSON Subcomponent for endpoint 'data?datasetid=YOUR_DATASETID'
-    """
+    Endpoint '/data?datasetid=YOUR_DATASETID' (subcomponent)
+    Represents a single data point in the response from the 'data?datasetid=YOUR_DATASETID' endpoint. (subcomponent)
+
+    Attributes:
+        date (str): The date and time of the observation, formatted as 'YYYY-MM-DDTHH:MM:SS'.
+        datatype (str): The type of data recorded (e.g., temperature, precipitation).
+        station (str): The identifier of the station where the data was recorded.
+        attributes (NotRequired[str]): Additional attributes or flags associated with the data point.
+        value (float | int): The recorded value of the data point.
+    """  # noqa: E501
 
     date: str  # Date as "YYYY-MM-DDTHH:MM:SS"
     datatype: str
@@ -221,8 +343,13 @@ class DatapointJSON(TypedDict):
 
 class DataJSON(TypedDict):
     """
-    Full JSON Response for endpoint 'data?datasetid=YOUR_DATASETID'
-    """
+    Endpoint '/data?datasetid=YOUR_DATASETID'
+    Represents the full JSON response structure for the '/data?datasetid=YOUR_DATASETID' endpoint.
+
+    Attributes:
+        metadata (MetadataJSON): Metadata information about the response, including pagination details.
+        results (list[DatapointJSON]): A list of data points returned by the query.
+    """  # noqa: E501
 
     metadata: MetadataJSON
     results: list[DatapointJSON]
