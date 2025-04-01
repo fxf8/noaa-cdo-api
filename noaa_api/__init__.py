580e973ed1a6509515c7bb851324adf19981ea98
Fuexfollets <fuex9follets@gmail.com>
2025-04-01 13:31:48 -0400

    Added selective json_response schemas

--- noaa_api/__init__.py
+++ noaa_api/__init__.py
@@ -1,14 +1,18 @@
 import importlib.metadata
 
+import noaa_api.json_responses as json_responses
 import noaa_api.json_schemas as json_schemas
 import noaa_api.parameter_schemas as parameter_schemas
 
 from .noaa import NOAAClient
 
+# Assign the selected schema attributes to the json_responses module
+
 __all__ = [
     "NOAAClient",
     "json_schemas",
     "parameter_schemas",
+    "json_responses",
 ]
 
 __version__ = importlib.metadata.version("noaa-api")
